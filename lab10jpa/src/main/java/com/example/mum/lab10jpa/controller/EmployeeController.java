f01da5fde261c42c664db9cba3606a7ef554c586
Sandeep Baniya <baniyasandy32@gmail.com>
2019-08-06 14:42:24 -0500

    lab10 new update

--- lab10jpa/src/main/java/com/example/mum/lab10jpa/controller/EmployeeController.java
+++ lab10jpa/src/main/java/com/example/mum/lab10jpa/controller/EmployeeController.java
@@ -4,12 +4,15 @@ package com.example.mum.lab10jpa.controller;
 import com.example.mum.lab10jpa.domain.Employee;
 import com.example.mum.lab10jpa.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.naming.Binding;
 import javax.validation.Valid;
@@ -38,6 +41,16 @@ public class EmployeeController {
 
         model.addAttribute("employees",employeeService.getAllEmployee());
         return "EmployeeDetails";
+    }
+
+    @GetMapping("/byPhone")
+    public String employeeByPhone(@RequestParam(value = "phoneNumber", required = false)String phoneNumber, Model model) throws Exception {
+        model.addAttribute("employeeSearch", employeeService.getEmployeeByPhone(phoneNumber));
+        if(employeeService==null)
+            throw new Exception("Not Found");
+
+        return "EmployeeByPhone";
+
     }
 
 
